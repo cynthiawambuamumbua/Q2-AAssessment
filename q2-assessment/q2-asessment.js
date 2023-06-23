4ee90e6c370b19024b353a9910ba36c122b8f0f3
priange <ityakup@gmail.com>
2023-06-23 06:57:04 +0300

    commit changes

--- q2-assessment/q2-asessment.js
+++ q2-assessment/q2-asessment.js
@@ -8,51 +8,77 @@
 // objects, and how inheritance might come into play if there are different types of
 // stories or storytellers.
 
-// input=length,moral-lessons,age-group
-// output=story,story-teller,translator
-// process=create a class;create the constructor,pass in the attributes,
-//  create methods and pass in the conditions
-class ancestralStories{
-    constructor(length,moralLessons,ageGroup){
-        this.length=length
-        this.moralLessons=moralLessons
-        this.ageGroup=ageGroup
-    }
-    Story(){
-        if(this.story==="short" && this.moralLessons==="many"){
-            return "the story was given by the aged"
-        }
-        else{
-            if(this.story==="long" && this.moralLessons==="little"){
-                return "the story was given by the youth"
-            }
-        }
-    }
-    storyTeller(){
-        if(this.ageGroup==="children" && this.length==="short"){
-            return "the story was given by the aged"
-        }
-        else{
-            if(this.ageGroup==="youth" && this.moralLessons==="long"){
-                return "the story was given by the youth"
-            }
-        }
-    }
-    Translator(){
-    if(this.ageGroup==="children" && this.length==="short"){
-        return "the story was given by the aged"
-    }
-    else{
-        if(this.ageGroup==="youth" && this.moralLessons==="long"){
-            return "the story was given by the youth"
-        }
-    }
-}
-}
- const story1=new ancestralStories("long","many",12)
-console.log(story1)
+
+//  input=length,moral-lessons,age-group,story
+//  output=story_objects,story-teller_objects,translator_objects
+// process=create a class Story,storyteller and translator;pass in the attributes that inherits from the class
+
+// class StoryTeller extends Story {
+//     constructor(title, moral_lesson, age_group, community_origin, storyteller_name, story_length) {
+//       super(title, moral_lesson, age_group, story_length);
+//       this.community_origin = community_origin;
+//       this.storyteller_name = storyteller_name;
+//     }
+//   }
+//   class Translator extends Story {
+//     constructor(title, moral_lesson, age_group, story_length, origin_language, new_language) {
+//       super(title, moral_lesson, age_group, story_length);
+//       this.origin_language = origin_language;
+//       this.new_language = new_language;
+//     }
+//   }
+//   const story = new Story("The cat and the mouse", "Greediness", 5, "10 pages");
+//   console.log(story.display_details());
+//   const storyteller = new StoryTeller("The tortoise and the rabbit", "Friendship", 8, "English", "Lilian", 200);
+//   console.log(storyteller.display_details());
+//   console.log(`Storyteller Name: ${storyteller.storyteller_name}`);
+//   console.log(`Community Origin: ${storyteller.community_origin}`);
+//   const translator = new Translator("Africana", "diversity", 6, "20 pages", "swahili", "English");
+//   console.log(translator.display_details());
+//   console.log(`Origin Language: ${translator.origin_language}`);
+//   console.log(`New Language: ${translator.new_language}`);
 
 
+  class Story {
+    constructor(story_length, moral_lessons, age_group, title) {
+      this.story_length = story_length;
+      this.moral_lessons = moral_lessons;
+      this.age_group = age_group;
+      this.title = title;
+    }
+    
+    display_details() {
+      console.log(`${this.title} is a story for children who are ${this.age_group} years old. It is ${this.story_length} long and has ${this.moral_lessons} moral lessons.`);
+    }
+  }
+  
+  class StoryTeller extends Story {
+    constructor(community_origin, storyteller_name, story_length, moral_lessons, age_group, title) {
+      super(story_length, moral_lessons, age_group, title);
+      this.community_origin = community_origin;
+      this.storyteller_name = storyteller_name;
+    }
+  }
+  
+  class Translator extends Story {
+    constructor(title, story_length, moral_lessons, age_group, origin_language, new_language) {
+      super(story_length, moral_lessons, age_group, title);
+      this.origin_language = origin_language;
+      this.new_language = new_language;
+    }
+  }
+  
+  const story = new Story(10, 3, 7, "The hare and elephant");
+  story.display_details();
+  
+  const storyteller = new StoryTeller("Kamba", "Cynthia Mumbua", 12, 4, 10, "How the hare was clever than elephant");
+  console.log(`${storyteller.storyteller_name} from ${storyteller.community_origin} tells the story of ${storyteller.title}.`);
+  storyteller.display_details();
+  
+  const translator = new Translator("Clever", 8, 2, 12, "Swahili", "English");
+  console.log(`The story '${translator.title}' which is ${translator.story_length} long and has ${translator.moral_lessons} moral lessons originally in ${translator.origin_language} has been translated to ${translator.new_language}.`);
+  translator.display_details();
+  
 // q2
 // **African Cuisine:** You're creating a recipe app specifically for African cuisine.
 // The app needs to handle recipes from different African countries, each with its
@@ -67,30 +93,30 @@ console.log(story1)
 //  process=create a class Recipe,pass in the attributes,create methods and their subclasses
 
 
-class Recipe{
-    constructor(ingredients,preparation_time,cooking_method,nutritional_information ){
-        this.ingredients=ingredients
-        this.nutritional_information=nutritional_information
-       this.cooking_method=cooking_method
-        this.self.preparation_time=preparation_time
-    }
-    recipes(){
-        if (this.ingredients=="chillie" && this.cooking_method=="boiling" && this.nutritional_information=="healthy" && this.preparation_time=="morning"){
-            console.log("the food is moroccanRecipe")
-        }
-         else if( this.cooking_method=="roasting" && this.ingredients=="meat" && this.nutritional_information=="healthier" && this.preparation_time=="late"){
-            console.log("the food was ethiopianRecipe")
-         }
+// class Recipe{
+//     constructor(ingredients,preparation_time,cooking_method,nutritional_information ){
+//         this.ingredients=ingredients
+//         this.nutritional_information=nutritional_information
+//        this.cooking_method=cooking_method
+//         this.self.preparation_time=preparation_time
+//     }
+//     recipes(){
+//         if (this.ingredients=="chillie" && this.cooking_method=="boiling" && this.nutritional_information=="healthy" && this.preparation_time=="morning"){
+//             console.log("the food is moroccanRecipe")
+//         }
+//          else if( this.cooking_method=="roasting" && this.ingredients=="meat" && this.nutritional_information=="healthier" && this.preparation_time=="late"){
+//             console.log("the food was ethiopianRecipe")
+//          }
          
-            else{
-                console.log("the food was nigerianRecipe")
-    }
+//             else{
+//                 console.log("the food was nigerianRecipe")
+//     }
     
-}
+// }
     
-}
- const recipe1=new Recipe("any","noon","boiling","healthy")
- console.log(recipe1)
+// }
+//  const recipe1=new Recipe("any","noon","boiling","healthy")
+//  console.log(recipe1)
 
 //  **Wildlife Preservation:** You're a wildlife conservationist working on a
 //  program to track different species in a national park. Each species has its own
@@ -99,48 +125,48 @@ class Recipe{
  
 //  create classes to model `Species`, `Predator`, `Prey`, etc., and think about how
 //  these classes might relate to each other through inheritance.
-class Animals{
-    constructor(species,characteristics,behaviours){
-        this.species=species
-        this.characteristics=characteristics
-        this.behaviours=behaviours
-    }
-    different_species(){
-        if (this.species=="predators" && this.behaviours=="migration"){
-            console.log(`the ${self.species} will ${self.behaviours} from one point to another`)
-        }
-    else if(this.species=="preys"&&this.characteristics=="stay"){
-        console.log(`the {self.species} will{self.characteristics}`)
-    }
-    else{
-        console.log("no predators nor preys")
+// class Animals{
+//     constructor(species,characteristics,behaviours){
+//         this.species=species
+//         this.characteristics=characteristics
+//         this.behaviours=behaviours
+//     }
+//     different_species(){
+//         if (this.species=="predators" && this.behaviours=="migration"){
+//             console.log(`the ${self.species} will ${self.behaviours} from one point to another`)
+//         }
+//     else if(this.species=="preys"&&this.characteristics=="stay"){
+//         console.log(`the {self.species} will{self.characteristics}`)
+//     }
+//     else{
+//         console.log("no predators nor preys")
      
-        }
-    }
-    predators(){
-        if (this.species=="predators" && this.behaviours=="migration"){
-            console.log(`the ${self.species} will ${self.behaviours} from one point to another`)
-        }
-    else if(this.species=="preys"&&this.characteristics=="stay"){
-        console.log(`the {self.species} will{self.characteristics}`)
-    }
-    else{
-        console.log("no predators nor preys")
+//         }
+//     }
+//     predators(){
+//         if (this.species=="predators" && this.behaviours=="migration"){
+//             console.log(`the ${self.species} will ${self.behaviours} from one point to another`)
+//         }
+//     else if(this.species=="preys"&&this.characteristics=="stay"){
+//         console.log(`the {self.species} will{self.characteristics}`)
+//     }
+//     else{
+//         console.log("no predators nor preys")
      
-        }
-    }
-    prey(){
-        if (this.species=="predators" && this.behaviours=="migration"){
-            console.log(`the ${self.species} will ${self.behaviours} from one point to another`)
-        }
-    else if(this.species=="preys"&&this.characteristics=="stay"){
-        console.log(`the {self.species} will{self.characteristics}`)
-    }
-    else{
-        console.log("no predators nor preys")
+//         }
+//     }
+//     prey(){
+//         if (this.species=="predators" && this.behaviours=="migration"){
+//             console.log(`the ${self.species} will ${self.behaviours} from one point to another`)
+//         }
+//     else if(this.species=="preys"&&this.characteristics=="stay"){
+//         console.log(`the {self.species} will{self.characteristics}`)
+//     }
+//     else{
+//         console.log("no predators nor preys")
      
-        }
-    }
-}
-prey1=new Animals()
-console.log({prey1})
+//         }
+//     }
+// }
+// prey1=new Animals()
+// console.log({prey1})
--- q2-assessment/q2-assessment.py
+++ q2-assessment/q2-assessment.py
@@ -9,34 +9,42 @@
 # stories or storytellers.
 
 # input=length,moral-lessons,age-group,story
-# output=story,story-teller,translator
-# process=create a class;create the constructor,pass in the attributes,
-# create methods and pass in the conditions
-class ancestralStrories:
-    def __init__(self,length,moral_lessons,age_group):
-        self.length=length
+# output=story_objects,story-teller_objects,translator_objects
+# process=create a class Story,storyteller and translator;pass in the attributes that inherits from the class
+class Story:
+    def __init__(self,story_length,moral_lessons,age_group,title):
+        self.story_length=story_length
         self.moral_lessons=moral_lessons
         self.age_group=age_group
-        self.story=[]
-    def Story(self):
-        if self.story=="oral" and self.story_teller=="old":
-            return f"the story was given to childern aged {self.age_group}"
-        elif self.story=="written" and self.story_teller=="teenage":
-            return f"the story was written for children aged {self.age_group}"
-        else :return f" no story"
-    def storyTeller(self):
-        if self.story.teller=="youth" and self.length=="short":
-            return f"the story was {self.length}"
-        elif self.story_teller=="old" and self.length=="long":
-            return f"the story was {self.length} "
-        else: return f" no story teller"
-    def Translator(self):
-        if self.age_group=="children" and self.story_teller=="learnt":
-            return f"the translator was for {self.age_group}"
-        else :return f"no translator"
-    ancestralStrories=Story("oral")
-    print(ancestralStrories)
+        self.title = title
+    def display_details(self):
+        print(f"{self.title}is a story for childern who are {self.age_group} years old.It is {self.story_length} long and has {self.moral_lessons} moral lessons")
+        
+class StoryTeller(Story):
+    def __init__(self,community_origin,storyteller_name,story_length,moral_lessons,age_group,tittle):
+        super().__init__(story_length,moral_lessons,age_group,tittle)
+        self.community_origin = community_origin
+        self.storyteller_name = storyteller_name
 
+class Translator(Story):
+    def __init__(self, title,story_length, moral_lessons, age_group,origin_language,new_language):
+        super().__init__(story_length, moral_lessons, age_group, title)
+        self.origin_language=origin_language
+        self.new_language=new_language
+
+
+story = Story(10, 3, 7, "The hare and elephant")
+story.display_details()
+
+storyteller = StoryTeller("Kamba", "Cynthia Mumbua", 12, 4, 10, "How the hare was clever than elephant")
+print(f"{storyteller.storyteller_name} from {storyteller.community_origin} tells the story of {storyteller.title}.")
+storyteller.display_details()
+
+translator = Translator("Clever ", "Swahili", "The Hare and the elephant", 8, 2, 12)
+print(f"The story {translator.title} which is {translator.story_length} long and has {translator.moral_lessons} moral lessons originally in {translator.origin_language} has been translated to {translator.new_language}.")
+translator.display_details()
+        
+    
 # q2
 # **African Cuisine:** You're creating a recipe app specifically for African cuisine.
 # The app needs to handle recipes from different African countries, each with its
@@ -49,22 +57,22 @@ class ancestralStrories:
 # output=types of recipes
 # process=create a class Recipe,pass in the attributes,create methods and their subclasses
 
-class Recipe:
-    def __init__(self,ingredients,preparation_time,cooking_method,nutritional_information):
-        self.ingredients=ingredients
-        self.nutritional_information=nutritional_information
-        self.cooking_method=cooking_method
-        self.preparation_time=preparation_time
-    def recipes(self):
-            if self.ingredients=="chillie" and self.cooking_method=="boiling" and self.nutritional_information=="healthy" and self.preparation_time=="morning":
-                print({f"the food was moroccanRecipe"})
-            elif self.cooking_method=="roasting" and self.ingredients=="meat" and self.nutritional_information=="healthier" and self.preparation_time=="late":
-                print({f"the food was ethiopianRecipe"})
-            else:
-                self.cooking_method=="mix" and self.ingredients=="floor" and self.nutritional_information=="low" and self.preparation_time=="evening"
-            print({f"the food was nigerianRecipe"})
-    recipes1=recipes()
-    print(recipes1)
+# class Recipe:
+#     def __init__(self,ingredients,preparation_time,cooking_method,nutritional_information):
+#         self.ingredients=ingredients
+#         self.nutritional_information=nutritional_information
+#         self.cooking_method=cooking_method
+#         self.preparation_time=preparation_time
+#     def recipes(self):
+#             if self.ingredients=="chillie" and self.cooking_method=="boiling" and self.nutritional_information=="healthy" and self.preparation_time=="morning":
+#                 print({f"the food was moroccanRecipe"})
+#             elif self.cooking_method=="roasting" and self.ingredients=="meat" and self.nutritional_information=="healthier" and self.preparation_time=="late":
+#                 print({f"the food was ethiopianRecipe"})
+#             else:
+#                 self.cooking_method=="mix" and self.ingredients=="floor" and self.nutritional_information=="low" and self.preparation_time=="evening"
+#             print({f"the food was nigerianRecipe"})
+#     recipes1=recipes()
+#     print(recipes1)
 
 # **Wildlife Preservation:** You're a wildlife conservationist working on a
 # program to track different species in a national park. Each species has its own
@@ -78,29 +86,29 @@ class Recipe:
 # input=species,characteristics,behaviours
 # output=different species,different behaviours
 # process=create class Animals,create constructor,create methods
-class Animals:
-    def __init__(self,species,characteristics,behaviours):
-        self.species=species
-        self.characteristics=characteristics
-        self.behaviours=behaviours
-    def different_species(self):
-        if self.species=="predators" and self.behaviours=="migration":
-            print({f"the {self.species} will {self.behaviours} from one point to another"})
-        elif self.species=="preys" and self.characteristics=="stay":
-            print({f"the {self.species} will{self.characteristics}"})
-        else:print({"no predators nor preys"})
+# class Animals:
+#     def __init__(self,species,characteristics,behaviours):
+#         self.species=species
+#         self.characteristics=characteristics
+#         self.behaviours=behaviours
+#     def different_species(self):
+#         if self.species=="predators" and self.behaviours=="migration":
+#             print({f"the {self.species} will {self.behaviours} from one point to another"})
+#         elif self.species=="preys" and self.characteristics=="stay":
+#             print({f"the {self.species} will{self.characteristics}"})
+#         else:print({"no predators nor preys"})
 
-    def predators(self):
-        if self.species=="predators" and self.behaviours=="migration":
-            print({f"the {self.species} will {self.behaviours} from one point to another"})
-        elif self.species=="preys" and self.characteristics=="stay":
-            print({f"the {self.species} will{self.characteristics}"})
-        else:print({"no predators nor preys"})
-    def preys(self):
-        if self.species=="predators" and self.behaviours=="migration":
-            print({f"the {self.species} will {self.behaviours} from one point to another"})
-        elif self.species=="preys" and self.characteristics=="stay":
-            print({f"the {self.species} will{self.characteristics}"})
-        else:print({"no predators nor preys"})
-    preys1=different_species("zebras")
-    print(preys1)
+#     def predators(self):
+#         if self.species=="predators" and self.behaviours=="migration":
+#             print({f"the {self.species} will {self.behaviours} from one point to another"})
+#         elif self.species=="preys" and self.characteristics=="stay":
+#             print({f"the {self.species} will{self.characteristics}"})
+#         else:print({"no predators nor preys"})
+#     def preys(self):
+#         if self.species=="predators" and self.behaviours=="migration":
+#             print({f"the {self.species} will {self.behaviours} from one point to another"})
+#         elif self.species=="preys" and self.characteristics=="stay":
+#             print({f"the {self.species} will{self.characteristics}"})
+#         else:print({"no predators nor preys"})
+#     preys1=different_species("zebras")
+#     print(preys1)
